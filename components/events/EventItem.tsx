f6e7fe238217de93d547e0b47a1dbb3cf81022ac
Jean Gabin Ishimwe <ij.gabin123@gmail.com>
2022-12-08 10:01:09 +0200

    passing data to ventItem Components

--- components/events/EventItem.tsx
+++ components/events/EventItem.tsx
@@ -1,8 +1,34 @@
+import Link from 'next/link';
 import React from 'react';
 import { DUMMY_EVENTS_INTERFACE } from '../../dummy-data';
 
 const EventItem: React.FC<{ item: DUMMY_EVENTS_INTERFACE }> = ({ item }) => {
-  return <li></li>;
+  const humanReadableDate = new Date(item.date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+  const formattedAddress = item.location.replace(', ', '\n');
+  const exploreLink = `/events/${item.id}`;
+  return (
+    <li>
+      <img src={'/' + item.image} alt={item.title} />
+      <div>
+        <div>
+          <h2>{item.title}</h2>
+          <div>
+            <time>{humanReadableDate}</time>
+          </div>
+          <div>
+            <address> {formattedAddress} </address>
+          </div>
+        </div>
+        <div>
+          <Link href={exploreLink}>Explore Event</Link>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default EventItem;
