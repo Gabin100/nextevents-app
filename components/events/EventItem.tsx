78eba8ecc91c52960607f37a3faa88dc8e26468a
Jean Gabin Ishimwe <ij.gabin123@gmail.com>
2022-12-08 10:27:43 +0200

    styling Components

--- components/events/EventItem.tsx
+++ components/events/EventItem.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import React from 'react';
 import { DUMMY_EVENTS_INTERFACE } from '../../dummy-data';
+import classes from './styles/event-item.module.css';
 
 const EventItem: React.FC<{ item: DUMMY_EVENTS_INTERFACE }> = ({ item }) => {
   const humanReadableDate = new Date(item.date).toLocaleDateString('en-US', {
@@ -11,19 +12,19 @@ const EventItem: React.FC<{ item: DUMMY_EVENTS_INTERFACE }> = ({ item }) => {
   const formattedAddress = item.location.replace(', ', '\n');
   const exploreLink = `/events/${item.id}`;
   return (
-    <li>
+    <li className={classes.item}>
       <img src={'/' + item.image} alt={item.title} />
-      <div>
-        <div>
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <h2>{item.title}</h2>
-          <div>
+          <div className={classes.date}>
             <time>{humanReadableDate}</time>
           </div>
-          <div>
+          <div className={classes.address}>
             <address> {formattedAddress} </address>
           </div>
         </div>
-        <div>
+        <div className={classes.actions}>
           <Link href={exploreLink}>Explore Event</Link>
         </div>
       </div>
--- components/events/EventList.tsx
+++ components/events/EventList.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { DUMMY_EVENTS_INTERFACE } from '../../dummy-data';
 import EventItem from './EventItem';
+import classes from './styles/event-list.module.css';
 
 const EventList: React.FC<{ items: DUMMY_EVENTS_INTERFACE[] }> = ({
   items,
 }) => {
   return (
-    <ul>
+    <ul className={classes.list}>
       {items.map((event) => (
         <EventItem key={event.id} item={event} />
       ))}
