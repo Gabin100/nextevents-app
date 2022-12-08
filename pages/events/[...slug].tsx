5693d4d7a56409d883bf6353895901635b563675
Jean Gabin Ishimwe <ij.gabin123@gmail.com>
2022-12-08 09:19:20 +0200

    setting up main pages

---
+++ pages/events/[...slug].tsx
@@ -0,0 +1,9 @@
+function FilteredEventsPage() {
+  return (
+    <div>
+      <h1>Filtered Events Page (Show filtered Events)</h1>
+    </div>
+  );
+}
+
+export default FilteredEventsPage;
---
+++ pages/events/[eventId].tsx
@@ -0,0 +1,9 @@
+function EventDetailPage() {
+  return (
+    <div>
+      <h1>Event Detail Page (Show selected Event)</h1>
+    </div>
+  );
+}
+
+export default EventDetailPage;
