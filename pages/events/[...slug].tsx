a52a3b1bccd835e8b944bfb3d91426b3b8da612e
Jean Gabin Ishimwe <ij.gabin123@gmail.com>
2022-12-09 07:29:19 +0200

    Extracting Data on the Catch-All Page

--- pages/events/[...slug].tsx
+++ pages/events/[...slug].tsx
@@ -1,7 +1,41 @@
+import { useRouter } from 'next/router';
+import EventList from '../../components/events/EventList';
+import { getFilteredEvents } from '../../dummy-data';
+
 function FilteredEventsPage() {
+  const router = useRouter();
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className='center'>Loading...</p>;
+  }
+
+  const filteredYear = Number(filterData[0]);
+  const filteredMonth = Number(filterData[1]);
+
+  if (
+    isNaN(filteredYear) ||
+    isNaN(filteredMonth) ||
+    filteredYear > 2030 ||
+    filteredYear < 2021 ||
+    filteredMonth < 1 ||
+    filteredMonth > 12
+  ) {
+    return <p className='center'>Invalid filter. Please adjust your values!</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: filteredYear,
+    month: filteredMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No Events found for the chosen filter!</p>;
+  }
+
   return (
     <div>
-      <h1>Filtered Events Page (Show filtered Events)</h1>
+      <EventList items={filteredEvents} />
     </div>
   );
 }
